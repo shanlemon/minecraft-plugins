a1a051e0c0f533da7128fde1b1bf9055ae1888a2
Shan Memon <shankmemon@gmail.com>
2020-02-02 20:06:55 -0600

    init

---
+++ one-in-the-chamber/src/main/java/com/shanmemon/javaplugin/Command.java
@@ -0,0 +1,19 @@
+package com.shanmemon.javaplugin;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Command implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            String command = "/oitc";
+            
+        }
+        return false;
+    }
+
+}
